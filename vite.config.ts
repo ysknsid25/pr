74c960ceffbec4ad8d09676fe4a4b25061a8556c
ysknsid25 <kengo071225@gmail.com>
2025-12-07 14:56:07 +0900

    chore: add permission
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import build from '@hono/vite-build/cloudflare-workers'
-import adapter from '@hono/vite-dev-server/node'
-import tailwindcss from '@tailwindcss/vite'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import build from "@hono/vite-build/cloudflare-workers";
+import adapter from "@hono/vite-dev-server/node";
+import tailwindcss from "@tailwindcss/vite";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    honox({
-      devServer: { adapter },
-      client: { input: ['/app/client.ts', '/app/style.css'] }
-    }),
-    tailwindcss(),
-    build()
-  ]
-})
+    plugins: [
+        honox({
+            devServer: { adapter },
+            client: { input: ["/app/client.ts", "/app/style.css"] },
+        }),
+        tailwindcss(),
+        build(),
+    ],
+});
