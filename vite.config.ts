ae9416d0cce7827be5064c7a3472c0d10e783479
ysknsid25 <kengo071225@gmail.com>
2025-12-07 17:42:04 +0900

    chore: runtime node
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import build from "@hono/vite-build/cloudflare-workers";
-import adapter from "@hono/vite-dev-server/cloudflare";
+import adapter from "@hono/vite-dev-server/node";
 import tailwindcss from "@tailwindcss/vite";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
