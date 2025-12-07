edb692894e27371598070f237247770418d42ad9
ysknsid25 <kengo071225@gmail.com>
2025-12-07 17:33:22 +0900

    chore: setting CD
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import build from "@hono/vite-build/cloudflare-workers";
-import adapter from "@hono/vite-dev-server/node";
+import adapter from "@hono/vite-dev-server/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
